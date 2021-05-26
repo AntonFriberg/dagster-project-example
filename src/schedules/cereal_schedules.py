60c11c823ec2bee062307e6467de0ea86d00f5b2
Anton Friberg <antonfr@axis.com>
2021-05-26 12:10:34 +0200

    Fix make sure schedule takes context as input

--- src/schedules/cereal_schedules.py
+++ src/schedules/cereal_schedules.py
@@ -7,10 +7,15 @@ from pipelines.cereal_pipelines import complex_pipeline  # pylint: disable=unuse
 
 # https://docs.dagster.io/concepts/partitions-schedules-sensors/schedules
 @schedule(
-    cron_schedule="0 9 * * 1-5",
+    cron_schedule="10 12 * * 1-5",
     pipeline_name="complex_pipeline",
     execution_timezone="Europe/Stockholm",
 )
-def every_weekday_9am():
+def every_weekday_9am(context):
     """Example of how to setup a weekday schedule for a pipeline."""
-    return {}
+    date = context.scheduled_execution_time.strftime("%Y-%m-%d")
+    return {
+        "solids": {
+            "download_cereals": {"config": {"date": date}}
+        }
+    }
