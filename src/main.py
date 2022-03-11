bb159ced95b6ad639f22c4b2af63dd58e00e9512
Anton Friberg <antonfr@axis.com>
2022-03-11 17:01:52 +0100

    Refactor main.py to new syntax
    Change-Id: I58340a89d9b25c05cae7c3ce85a3effee4462146

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
 """Example of how to run a Dagster pipeline from normal Python script."""
-from dagster import execute_pipeline
-
 from pipelines.cereal_pipelines import complex_pipeline
 
 if __name__ == "__main__":
-    result = execute_pipeline(complex_pipeline)
+    result = complex_pipeline.execute_in_process()
