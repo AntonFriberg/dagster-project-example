967da836927091ea3e7de010f487487452450832
Anton Friberg <antonfr@axis.com>
2022-03-11 17:22:32 +0100

    Update repositories to new syntax
    Change-Id: I1996be564c0fdb5614811de7356515d59f467c0d

--- src/repositories/cereal_repositories.py
+++ src/repositories/cereal_repositories.py
@@ -1,17 +1,11 @@
 """Collection of Cereal repositories"""
 from dagster import repository
 
-from pipelines.cereal_pipelines import complex_pipeline, hello_cereal_pipeline
+from jobs.cereal_jobs import complex_job, hello_cereal_job
 from schedules.cereal_schedules import every_weekday_9am
 
 
 @repository
 def hello_cereal_repository():
-    """Collection of cereal pipelines and other definitions used by Dagster."""
-    return {
-        "pipelines": {
-            "hello_cereal_pipeline": lambda: hello_cereal_pipeline,
-            "complex_pipeline": lambda: complex_pipeline,
-        },
-        "schedules": {"every_weekday_9am": lambda: every_weekday_9am},
-    }
+    """Collection of cereal jobs and other definitions used by Dagster."""
+    return [complex_job, hello_cereal_job, every_weekday_9am]
