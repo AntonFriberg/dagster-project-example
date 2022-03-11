3a04ced5da529750362ca6e2a8e06dcdd380ea8e
Anton Friberg <antonfr@axis.com>
2022-03-11 17:15:21 +0100

    Change from pipelines to jobs in jobs unit-tests
    Change-Id: Idb43ba8a342f2fb8c47aaaecfcd072a146784350

--- src/tests/test_cereal_jobs.py
+++ src/tests/test_cereal_jobs.py
@@ -1,23 +1,23 @@
-"""Example of how to unit-test Dagster pipelines."""
+"""Example of how to unit-test Dagster ops."""
 from dagster import ExecuteInProcessResult
 
-from jobs.cereal_jobs import complex_pipeline, hello_cereal_pipeline
+from jobs.cereal_jobs import complex_op, hello_cereal_op
 
 
-def test_hello_cereal_pipeline():
-    """Example of a unit test of a simple pipeline."""
-    result = hello_cereal_pipeline.execute_in_process()
+def test_hello_cereal_op():
+    """Example of a unit test of a simple op."""
+    result = hello_cereal_op.execute_in_process()
     assert isinstance(result, ExecuteInProcessResult)
     assert result.success
     assert len(result.output_for_node("hello_cereal")) == 77
 
 
-def test_complex_pipeline():
-    """Example of a unit test of a more complex pipeline."""
-    result = complex_pipeline.execute_in_process()
+def test_complex_op():
+    """Example of a unit test of a more complex op."""
+    result = complex_op.execute_in_process()
     assert isinstance(result, ExecuteInProcessResult)
     assert result.success
-    # inspect individual solid result
+    # inspect individual ops result
     assert len(result.output_for_node("download_cereals")) == 77
     assert result.output_for_node("find_highest_calorie_cereal") == "Strawberry Fruit Wheats"
     assert result.output_for_node("find_highest_protein_cereal") == "Special K"
