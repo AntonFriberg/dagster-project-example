d0469bfd632c6b5f0e9af9dd03cc329ed5ffc418
Anton Friberg <antonfr@axis.com>
2022-03-11 17:00:50 +0100

    Refactor cereal_solids to new syntax
    Change-Id: I42e6788a595ecf2cb9c7dc22117d27664b6f0535

--- src/solids/cereal_solids.py
+++ src/solids/cereal_solids.py
@@ -2,46 +2,50 @@
 import csv
 
 import requests
-from dagster import solid
+from dagster import op, get_dagster_logger
 
 
-@solid
-def hello_cereal(context):
-    """Example of a Dagster solid that retrieves data from HTTP source."""
-    # Assuming the dataset is in the same directory as this file
+@op
+def hello_cereal():
+    """Example of a Dagster op that retrieves data from HTTP source."""
     response = requests.get("https://docs.dagster.io/assets/cereal.csv")
     lines = response.text.split("\n")
     cereals = list(csv.DictReader(lines))
-
-    context.log.info("Found {n_cereals} cereals".format(n_cereals=len(cereals)))
-
+    get_dagster_logger().info(f"Found {len(cereals)} cereals")
     return cereals
 
 
-@solid
+@op
 def download_cereals():
-    """Example of a Dagster solid that returns a list of objects."""
+    """Example of a Dagster op that returns a list of objects."""
     response = requests.get("https://docs.dagster.io/assets/cereal.csv")
     lines = response.text.split("\n")
     return list(csv.DictReader(lines))
 
 
-@solid
+@op
 def find_highest_calorie_cereal(cereals):
-    """Example of a Dagster solid that takes input and produces output."""
-    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal["calories"]))
-    return sorted_cereals[-1]["name"]
+    """Example of a Dagster op that takes input and produces output."""
+    sorted_by_calorie = list(sorted(cereals, key=lambda cereal: cereal["calories"]))
+    get_dagster_logger().info(
+        f'{sorted_by_calorie[-1]["name"]} is the cereal that contains the most calories'
+    )
+    return sorted_by_calorie[-1]["name"]
 
 
-@solid
+@op
 def find_highest_protein_cereal(cereals):
-    """Example of a Dagster solid that takes input and produces output."""
-    sorted_cereals = list(sorted(cereals, key=lambda cereal: cereal["protein"]))
-    return sorted_cereals[-1]["name"]
+    """Example of a Dagster op that takes input and produces output."""
+    sorted_by_protein = list(sorted(cereals, key=lambda cereal: cereal["protein"]))
+    get_dagster_logger().info(
+        f'{sorted_by_protein[-1]["name"]} is the cereal that contains the most protein'
+    )
+    return sorted_by_protein[-1]["name"]
 
 
-@solid
-def display_results(context, most_calories, most_protein):
-    """Example of a Dagster solid that takes inputs but does not produce output."""
-    context.log.info(f"Most caloric cereal: {most_calories}")
-    context.log.info(f"Most protein-rich cereal: {most_protein}")
+@op
+def display_results(most_calories, most_protein):
+    """Example of a Dagster op that takes inputs but does not produce output."""
+    logger = get_dagster_logger()
+    logger.info(f"Most caloric cereal: {most_calories}")
+    logger.info(f"Most protein-rich cereal: {most_protein}")
