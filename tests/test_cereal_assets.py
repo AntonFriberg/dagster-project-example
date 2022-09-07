c1b45c74a03a93a7e914ad3261e45e19c05d2f37
Anton Friberg <antonfr@axis.com>
2022-09-07 17:31:05 +0200

    Add an example of mocked requests to unit-tests
    Change-Id: Ie612d70f3c02134c0049dc4f10e5d1ed9df32b56

--- tests/test_cereal_assets.py
+++ tests/test_cereal_assets.py
@@ -1,6 +1,15 @@
 """Example of how to unit-test Dagster assets."""
 
-from dagster_example.assets.cereal import cereals, highest_calorie_cereal, highest_protein_cereal
+from pathlib import Path
+
+from dagster_example.assets.cereal import (
+    CEREAL_URL,
+    cereals,
+    highest_calorie_cereal,
+    highest_protein_cereal,
+)
+
+MOCKED_CEREAL_FILE = Path(__file__).parent / "misc/cereals.csv"
 
 TEST_CEREALS = [
     {"name": "All-Bran", "calories": 70, "protein": 4},
@@ -9,8 +18,24 @@ TEST_CEREALS = [
 ]
 
 
+def get_mocked_cereal_text():
+    """Read data from csv file."""
+    with open(MOCKED_CEREAL_FILE, "r", encoding="utf-8") as file:
+        csv_text = file.read()
+    return csv_text
+
+
 def test_cereals_asset():
     """Example of a unit-test of an asset."""
+    res = cereals()
+    assert isinstance(res, list)
+    assert len(res) == 77
+
+
+def test_cereals_asset_mocked(requests_mock):
+    """Example of a unit-test of an asset with mocked request."""
+    dummy_cereal_text = get_mocked_cereal_text()
+    requests_mock.get(CEREAL_URL, text=dummy_cereal_text)
     res = cereals()
     assert isinstance(res, list)
     assert len(res) == 77
