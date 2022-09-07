6ac9ef08a58cbbb0a9da9cd44c551d32ed0586c6
Anton Friberg <antonfr@axis.com>
2022-09-07 16:02:38 +0200

    Refactor dagster jobs into a single module
    After using Dagster for a while it seems I rarely need more than a
    single Dagster job module that contain my configured graphs.
    
    Change-Id: I06c52a49b08c2c8aae86bed8d131a7a29bb3d528

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 """Example of how to run a Dagster op from normal Python script."""
-from jobs.cereal_jobs import complex_job
+from jobs import complex_job
 
 if __name__ == "__main__":
     result = complex_job.execute_in_process()
